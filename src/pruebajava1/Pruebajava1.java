1ea9be19577d5986e7e71eb373a5fee63698a70a
isabel62 <isabeliribarreng@hotmail.com>
2017-10-30 20:53:09 -0300

    pproyesto de operacion del un triangulo

---
+++ src/pruebajava1/Pruebajava1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+/*
+ 
+
+  @author LAB08 ´{
+// Metodo de costructor
+public class Pruebajava1 { 
+  int Area ;
+  int base ;
+  int Altura;
+
+} 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(stringtln [] args) {
+      string out println("Por favor ingrese Area")
+      string out println("Por favor ingrese Base")
+      string out println(" po favor ingrese Altura")
+    //Aplicacion del proyecto
+        // TODO code application logic hare
+      
+    }
+    floatArea=restangulo
+    floatBase=restangulo
+    FloatAltura=restangulo
+intA.4
+intB.5
+int.("4x5"=20mtrs)/multiplicasion de Base y Altura
+int.("Area de restangulo= 20mts")
+int.(4,A x 5,B)<20mtrs=total /desarrollo la operacion
+// Conclucion  del  desarrollo
+}
